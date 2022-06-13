76a7688a71a6e3ef3b86116ae8a621105b4d434b
Kaburu12 <michael.kaburur@student.moringaschool.com>
2022-06-13 18:02:18 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector("form");
+    form.addEventListener("submit", (e) => {
+    e.preventDefault()
+    taskLister(e.target.new-task-description.value)
+  })
 });
+
+function taskLister(task) {
+  let li = document.createElement("li");
+  let btn = document.createElement("button");
+  li.textContent=`${task}`;
+  btn.textContent = ("x");
+  btn.addEventListener('click', handleDelete);
+  li.appendChild(btn);
+  console.log('li')
+  document.querySelector("#tasks").appendChild(li)
+
+}
+
+function handleDelete(e){
+  e.target.parentNode.remove()
+}
